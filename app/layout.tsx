fe4f65936d5d7e11c9e1fbb86725d330dd07baa9
dinden <arleguidaniel@gmail.com>
2023-12-16 12:29:16 +0900

    Update layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header';
-import './globals.css';
 import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,8 +18,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-rem]"></div>
+        <div className="bg-[#bae6fd] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-[#bfdbfe] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left[-rem]"></div>
 
         <Header />
         {children}
@@ -27,5 +27,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-//TimeStamp: 1:33:40
