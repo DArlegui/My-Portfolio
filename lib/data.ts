09a78a0b4bb74ead7fbb67f5504ae2f2b55e22e2
dinden <arleguidaniel@gmail.com>
2023-12-18 05:49:32 +0900

    Update project titles and images

--- lib/data.ts
+++ lib/data.ts
@@ -69,18 +69,18 @@ export const projectsData = [
     imageUrl: traceImg,
   },
   {
-    title: 'Game Hub',
+    title: 'Weather App',
     description:
       'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: gamehubImg,
+    imageUrl: weatherImg,
   },
   {
-    title: 'Weather App',
+    title: 'Game Hub',
     description:
       'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
     tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: weatherImg,
+    imageUrl: gamehubImg,
   },
 ] as const;
 
