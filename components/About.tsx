2340073756facf9b515346babd2ef661b32be9fb
dinden <arleguidaniel@gmail.com>
2023-12-19 23:35:48 +0900

    Added id attributes to components and smooth scrolling class.

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,8 @@ export default function About() {
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}>
+      transition={{ delay: 0.175 }}
+      id="about">
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{' '}
