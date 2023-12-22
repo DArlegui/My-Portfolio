54f3a3f9d2c088c0f63424f7fb05f0d82803258c
dinden <arleguidaniel@gmail.com>
2023-12-22 15:27:28 +0900

    Add sharp package and update webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: [
-  //   {
-  //     protocol: 'https',
-  //     hostname: 'images.unsplash.com',
-  //   },
-  // ],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack as an argument to the function
+    // which is required to use webpack properties
+    config.cache = false;
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
+
+//Old
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // images: [
+//   //   {
+//   //     protocol: 'https',
+//   //     hostname: 'images.unsplash.com',
+//   //   },
+//   // ],
+// };
+
+// module.exports = nextConfig;
