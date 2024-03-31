ee9d32439e0a81b75060e00ab883bf2fb9b40bd4
darlegui <arleguidaniel@gmail.com>
2024-03-30 18:23:10 -0700

    Setup resend and email functionalities for Contact

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,5 +1,33 @@
 'use server';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const validateString = (str: unknown, maxLength: number) => {
+  if (!str || typeof str !== 'string' || str.length > maxLength) {
+    return false;
+  }
+  return true;
+};
 
 export const sendEmail = async (formData: FormData) => {
-  console.log(formData.get('senderEmail'), formData.get('message'));
+  const message = formData.get('message');
+  const senderEmail = formData.get('senderEmail');
+
+  //Server-side validation
+  if (!validateString(senderEmail, 50) || !validateString(message, 5000)) {
+    return { error: 'Invalid input' };
+  }
+
+  try {
+    await resend.emails.send({
+      from: 'Contact Form <onboarding@resend.dev>',
+      to: 'arleguidaniel@gmail.com',
+      subject: 'Message from Portfolio Website',
+      reply_to: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
           await sendEmail(formData);
         }}>
         <input className="h-14 px-4 rounded-lg borderBlack outline-gray-400" name="senderEmail" type="email" required={true} maxLength={50} placeholder="Your Email" />
-        <textarea className="h-52 my-3 rounded-lg borderBlack p-4 outline-gray-400" name="message" required={true} maxLength={500} placeholder="Your Message" />
+        <textarea className="h-52 my-3 rounded-lg borderBlack p-4 outline-gray-400" name="message" required={true} maxLength={5000} placeholder="Your Message" />
         <button
           className="group flex items-center justify-center gap-2 h-[2.5rem] w-[6.7rem] bg-gray-700 text-white rounded-xl outline-none transition-all focus:scale-105 hover:bg-gray-900 hover:scale-105 active:scale-110"
           type="submit">
