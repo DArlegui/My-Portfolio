3e40c58e328963bf96295e37b95d375a41c8ecca
darlegui <arleguidaniel@gmail.com>
2024-04-02 12:22:50 -0700

    Added react-email to customize senderEmail

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,5 +1,7 @@
 'use server';
+import ContactFormEmail from '@/email/ContactFormEmail';
 import { getErrorMessage, validateString } from '@/lib/utils';
+import React from 'react';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -19,7 +21,7 @@ export const sendEmail = async (formData: FormData) => {
       to: 'arleguidaniel@gmail.com',
       subject: 'Message from Portfolio Website',
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, { message: message as string, senderEmail: senderEmail as string }),
     });
   } catch (error: unknown) {
     return {
