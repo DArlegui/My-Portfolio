51ba23d6160752f0d206ebd01c1fa24e634db119
darlegui <arleguidaniel@gmail.com>
2024-04-19 15:34:04 -0700

    Took off motion for Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,14 +10,19 @@ export default function Header() {
 
   return (
     <header className="z-[999] relative">
-      <motion.div
+      <div
         className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] "
-        initial={{ y: -100, x: '-50%', opacity: 0 }}
-        animate={{ y: 0, x: '-50%', opacity: 1 }}></motion.div>
+        // initial={{ y: -100, x: '-50%', opacity: 0 }}
+        // animate={{ y: 0, x: '-50%', opacity: 1 }}
+      ></div>
       <nav className=" flex fixed top-0 left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[0.9rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.95rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
-            <motion.li className="h-3/4 flex items-center justify-center relative" key={link.hash} initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+            <li
+              className="h-3/4 flex items-center justify-center relative"
+              key={link.hash}
+              // initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}
+            >
               <Link
                 className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition', {
                   'text-gray-950': activeSection === link.name,
@@ -30,17 +35,18 @@ export default function Header() {
                 {link.name}
 
                 {link.name === activeSection && (
-                  <motion.span
+                  <span
                     className="bg-gray-100 rounded-full absolute inset-0 -z-10"
-                    layoutId="activeSection"
-                    transition={{
-                      type: 'spring',
-                      stiffness: 380,
-                      damping: 30,
-                    }}></motion.span>
+                    // layoutId="activeSection"
+                    // transition={{
+                    //   type: 'spring',
+                    //   stiffness: 380,
+                    //   damping: 30,
+                    // }}
+                  ></span>
                 )}
               </Link>
-            </motion.li>
+            </li>
           ))}
         </ul>
       </nav>
