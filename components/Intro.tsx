5dd5a7a3ba0d3c6d4e89ac4fe651a006b93e76d1
darlegui <arleguidaniel@gmail.com>
2024-04-19 18:19:17 -0700

    Updated Intro's Button

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,14 +4,19 @@ import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { FaGithubSquare } from 'react-icons/fa';
-import { HiDownload } from 'react-icons/hi';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { PiCertificateBold } from 'react-icons/pi';
 import photo from '../public/photo.jpg';
+
+const certLink = process.env.CERTS_LINK;
 
 export default function Intro() {
   const { ref } = useSectionInView('Home', 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+  const [showDocument, setShowDocument] = useState(false);
 
   return (
     <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
@@ -24,13 +29,7 @@ export default function Intro() {
               type: 'spring',
               duration: 0.2,
             }}>
-            <Image
-              src={photo}
-              alt="My Portrait"
-              quality="95"
-              priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl sm:h-36 sm:w-36"
-            />
+            <Image src={photo} alt="My Portrait" quality="95" priority={true} className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl sm:h-36 sm:w-36" />
           </motion.div>
           {/* <motion.span
             className="absolute bottom-2 right-2 text-4xl"
@@ -48,8 +47,9 @@ export default function Intro() {
       </div>
 
       <motion.h1 className="mb-10 mt-4 px-5 text-2xl font-medium !leading-[1.5] sm:text-[34px]" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
-        <span className="font-bold">Hi! I'm Daniel.</span> I'm a <span className="font-bold">full-stack developer</span> with <span className="font-bold"> 4 years</span> of
-        experience. Recently, I have been focusing on <span className="underline">web app development</span>.
+        <span className="font-bold">Hi! I'm Daniel.</span> I'm a <span className="font-bold">full-stack developer</span> with <span className="font-bold"> 4 years</span> of experience. Recently, I
+        have been focusing on web app development.
+        {/* <span className="underline">web app development</span>. */}
       </motion.h1>
 
       <motion.div
@@ -66,27 +66,41 @@ export default function Intro() {
           }}>
           Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-        <a
+
+        <button
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full 
-          outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer borderBlack
+          outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack
         "
-          href="/CV.pdf"
-          download>
-          Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
-          href="https://www.linkedin.com/in/danielarlegui/"
-          target="_blank">
-          <BsLinkedin />
-        </a>
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition cursor-pointer borderBlack"
-          href="https://github.com/DArlegui"
-          target="_blank">
-          <FaGithubSquare />
-        </a>
+          onClick={() => setShowDocument(!showDocument)}>
+          {showDocument ? 'Close' : 'View'} Resume <FaMagnifyingGlass className="opacity-60" />
+        </button>
+
+        <div className="flex flex-row items-center justify-center gap-3">
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
+            href="https://www.linkedin.com/in/danielarlegui/"
+            target="_blank">
+            <BsLinkedin />
+          </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition cursor-pointer borderBlack"
+            href="https://github.com/DArlegui"
+            target="_blank">
+            <FaGithubSquare />
+          </a>
+          <a
+            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition cursor-pointer borderBlack"
+            href={certLink}>
+            <PiCertificateBold />
+          </a>
+        </div>
       </motion.div>
+
+      {showDocument && (
+        <div className="flex flex-col items-center justify-center mt-10">
+          <iframe src="/CV.pdf" width="100%" height="600px"></iframe>
+        </div>
+      )}
     </section>
   );
 }
